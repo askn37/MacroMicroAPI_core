e01a60f2ac0cd783e0f7e1230ce095bd55642a02
朝日薫 / K.Sato <askn37@users.noreply.github.com>
2026-08-10 09:57:29 +0900

    Addition of "usrdef.h" template (#2)

---
+++ usrdef.h
@@ -0,0 +1,27 @@
+/**
+ * @file userdef
+ * @author askn (K.Sato) multix.jp
+ * @brief User-defined configuration file template
+ * @version 0.1
+ * @date 2026-08-09
+ *
+ * @copyright Copyright (c) 2026 askn37 at github.com
+ *
+ */
+/* MIT License : https://askn37.github.io/LICENSE.html */
+
+#pragma once
+
+/*
+ * This file is a template for a header file
+ * that is loaded using the following syntax.
+ *
+ * #include "usrdef.h"
+ * 
+ * If the header file exists in the current directory,
+ * it is loaded in preference to others.
+ * If it does not exist, this template is loaded instead,
+ * and no error occurs.
+ */
+
+ // end of header
